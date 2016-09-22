f2fd27b9d842a3d21c046f003875340d5a67934d
vknez95 <veljko.knezevic@gmail.com>
2016-09-21 22:45:36 -0400

    Html and Routing in the SPA

--- Startup.cs
+++ Startup.cs
@@ -54,6 +54,8 @@ namespace trails
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
+
+                routes.MapRoute("spa-routes", "{*anything}", new { controller = "Home", action = "Index" });
             });
         }
     }
